a0b423a3250217387f79e115a6ea2eecd5256415
mubashir-cvr <mubashircheruvayoor@gmail.com>
2025-03-11 13:35:50 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import GoogleSheetData from './results/resultPage'
+import GoogleSheetData from './results/ResultPage'
 
 function App() {
   const [count, setCount] = useState(0)
